d64fb940c5815dd1082300c81ec25c27595799b7
rechedYR <166982379+Cold-Script@users.noreply.github.com>
2024-11-18 13:17:14 +0700

    Update and rename README.md to youhub_loader_doors.lua

---
+++ youhub_loader_doors.lua
@@ -0,0 +1,11 @@
+local ArrayField = loadstring(game:HttpGet("https://raw.githubusercontent.com/Hosvile/Refinement/main/MC%3AArrayfield%20Library"))()
+local Window = ArrayField:CreateWindow({
+        Name = "YOUHUB - DOORS",
+        LoadingTitle = "v5",
+        LoadingSubtitle = "by rechedmcvn",
+        ConfigurationSaving = {
+            Enabled = true,
+            FolderName = nil, -- Create a custom folder for your hub/game
+            FileName = "ArrayField"
+	}
+})
